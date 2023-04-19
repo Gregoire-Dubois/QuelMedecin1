a2aa04dd0e699f4216b8b6909e496716bee079c2
Gregoire-Dubois <65712692+Gregoire-Dubois@users.noreply.github.com>
2023-04-19 15:04:46 +0200

    probleme sur date de naissance réglé

--- src/main/java/fr/eni/ecole/quelMedecin/test/TestPatient.java
+++ src/main/java/fr/eni/ecole/quelMedecin/test/TestPatient.java
@@ -2,12 +2,15 @@ package fr.eni.ecole.quelMedecin.test;
 
 import fr.eni.quelMedecin.Patient;
 
+import java.time.LocalDate;
+
 public class TestPatient {
 
     public static void main(String[] args) {
 
         Patient lePatient = new Patient("vambuche", "Hillary", "09..",
-                'm', 28706024, 1988,02,01);
+                'm', 28706024, LocalDate.of(1988,3,5));
+
         lePatient.afficher();
 
     }
